6b5461323d2ea0113166c5904e3aa958cdb01abd
kcwang1962 <48607107+kcwang1962@users.noreply.github.com>
2025-03-06 08:31:13 +0800

    00000

--- test/來自佳芳的訊息/script.js
+++ test/來自佳芳的訊息/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = '｢2025重生之旅｣線上課──3月29日開課'
+const text = '｢2025重生之旅｣線上課─3月29日開課'
 let idx = 1
 let speed = 300 / speedEl.value
 
